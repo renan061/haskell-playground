3f84e848f9e2eae920eb68f42cf3dfe62c1defbe
Renan <renan.061@gmail.com>
2017-10-25 23:36:19 -0200

    Adds more examples for the parser

--- parser.hs
+++ parser.hs
@@ -181,12 +181,16 @@ main = do lprint (lparser "x    ")
           lprint (lparser "x y")
           lprint (lparser "(/x -> x y) x")
           lprint (lparser "(/x -> /y -> x y) a b")
-          lprint (lparser "(a))") -- unmatched parentheses
           lprint (lparser "x #comentário")
           lprint (lparser "(/x -> /y -> x y) a b # mais comentário")
           lprint (lparser "(/x -> /y -> x y) a b # mais # comentário dentro")
-
-
-
-
+          lprint (lparser "(/x -> /y -> x y z) a b # mais # comentário dentro")
+          lprint (lparser "x (y z)")
+          lprint (lparser "x y z")
+          lprint (lparser "a b c d")
+          lprint (lparser "a (b c) d")
+          lprint (lparser "(/n -> n) 10")
+          lprint (lparser "/n -> n 10")
+          lprint (lparser "xyz10 abc11")
+          lprint (lparser "(a))") -- unmatched parentheses
 
